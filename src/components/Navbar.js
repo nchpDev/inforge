2d94b1c7691b871781b4244b7556a6a817997468
nchp7 <nacho.porretta00@gmail.com>
2024-02-03 23:11:29 -0300

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import '../styles/Navbar.css';
+import inforge_logo from '../assets/inforge_logo.webp';
 
 const Navbar = () => {
   return (
@@ -11,32 +12,25 @@ const Navbar = () => {
       <div className="navbar-container">
         <div className="logo-container">
           <a href="/">
-            <img id='nchp-logo' src="" alt="Logo" />
+            <img id="inforge-logo" src={inforge_logo} alt="Logo" />
           </a>
         </div>
         <div className="nav-links">
-          <Link to="services">
-            Services
-          </Link>
-          <Link to="who-we-are">
-            Who We Are
-          </Link>
-          <Link to="contact-us">
-            Contact Us
-          </Link>
-          <Link to="blog">
-            Blog
-          </Link>
-          <Link to="customer-login">
-            Customer Login
-          </Link>
-          <Link to="free-consultation">
-            Free Consultation
-          </Link>
+
+          <Link to="contact-us">Contact Us</Link>
+
+          <Link to="blog">Blog</Link>
+
+          <Link to="customer-login">Customer Login</Link>
+
+          <Link to="free-consultation">Free Consultation</Link>
+
           <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedinIn} />
           </a>
+
         </div>
+
         <div className="menu-icon" >
 
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../styles/Home.css';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
-import WhoWeAre from '../components/WhoWeAre';
 import Stories from '../components/Stories';
 import Footer from '../components/Footer';
 
@@ -11,7 +10,6 @@ const Home = () => {
     <div className="Home">
       <Navbar />
       <Hero />
-      <WhoWeAre />
       <Stories />
       <Footer />
     </div>
