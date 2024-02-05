3ba8a3fe07ba8aa38305db5cec3a83b1a2b7adbb
nchp <nacho.porretta00@gmail.com>
2024-02-05 13:03:21 -0300

    updates on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { Link as ScrollLink } from 'react-scroll';
 import { Link as RouterLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import inforge_logo from '../assets/inforge_logo.webp';
 
 const Navbar = () => {
@@ -16,26 +17,31 @@ const Navbar = () => {
             <img id="inforge-logo" src={inforge_logo} alt="Logo" />
           </a>
         </div>
+
         <div className="nav-links">
 
           <ScrollLink to="about-us" smooth={true} duration={500}>About Us</ScrollLink>
 
+          <RouterLink to="/blog">Blog</RouterLink>
+          
           <RouterLink to="/subscribe">Contact Us</RouterLink>
 
-          <RouterLink to="/blog">Blog</RouterLink>
+          <a href="https://support.inforge.us/s/login/?ec=302&startURL=%2Fs%2F" target="_blank" rel="noopener noreferrer">
+            Customer Login
+          </a>
 
-          <a href="https://www.external-consultation-page.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://help.inforge.us/s/" target="_blank" rel="noopener noreferrer">
             Free Consultation
           </a>
 
           <a href="https://www.linkedin.com/company/inforge-us/" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedinIn} />
+            <FontAwesomeIcon icon={faLinkedinIn} size='xl' />
           </a>
 
         </div>
 
         <div className="menu-icon" >
-
+          <FontAwesomeIcon icon={faBars} style={{color: "#000",}} />
         </div>
       </div>
     </nav>
