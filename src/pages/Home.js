c9c9f6c6e996991eff3c1d6bdde4ee52fc35bd27
nchp <nacho.porretta00@gmail.com>
2024-02-19 20:35:14 -0300

    updates on services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Home.css';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import Services from '../components/Services';
 import Stories from '../components/Stories';
 import Footer from '../components/Footer';
 
@@ -10,6 +11,7 @@ const Home = () => {
     <div className="Home">
       <Navbar />
       <Hero />
+      <Services />
       <Stories />
       <Footer />
     </div>
